d127148b6c981febadebf5c810113ceb2c22c493
aawanRahman <awanrahman55@gmail.com>
2021-07-24 00:14:16 +0600

    My account

---
+++ BookStore/src/main/java/com/bookstore/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.bookstore.config;
+
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+   
+	@Autowired
+	private Environment environment;
+	
+}
