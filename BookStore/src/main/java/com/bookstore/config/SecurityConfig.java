e3fbac168f8a3dca8f6a9a1b20da281f2136008a
aawanRahman <awanrahman55@gmail.com>
2021-07-25 22:27:58 +0600

    adding security configuration

--- BookStore/src/main/java/com/bookstore/config/SecurityConfig.java
+++ BookStore/src/main/java/com/bookstore/config/SecurityConfig.java
@@ -3,15 +3,81 @@ package com.bookstore.config;
 import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.bookstore.service.impl.UserSecurityService;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
    
 	@Autowired
 	private Environment environment;
+	
+	@Autowired
+	private UserSecurityService userSecurityService;
+	
+	private BCryptPasswordEncoder passwordEncoder() {
+		return SecurityUtility.passwordEncoder();
+	}
+	
+	private static final String[] PUBLIC_MATCHES  = {
+			
+			"/css/**",
+			"/js/**",
+			"/image/**",
+			"/",
+			"/myAccount"
+	};
+
+	
+	protected void configue(HttpSecurity http) throws Exception {
+		
+		http
+			.authorizeRequests()
+			.antMatchers(PUBLIC_MATCHES)
+			.permitAll()
+			.anyRequest()
+			.authenticated();
+		
+		
+		http
+			.csrf().disable().cors().disable()
+			.formLogin().failureUrl("/login?error").defaultSuccessUrl("/")
+			.loginPage("/login").permitAll()
+			.and()
+			.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.logoutSuccessUrl("/?logout")
+			.deleteCookies("remember-me")
+			.permitAll()
+			.and()
+			.rememberMe(); 
+	}
+	
+	 @Override
+	    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	        auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
+	    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
---
+++ BookStore/src/main/java/com/bookstore/service/impl/UserSecurityService.java
@@ -0,0 +1,30 @@
+package com.bookstore.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.domain.User;
+import com.bookstore.repository.UserRepository;
+
+@Service
+public class UserSecurityService  implements UserDetailsService{
+	@Autowired
+	private UserRepository userRepository;
+	
+	public UserDetails loadUserByUsername( String username) throws UsernameNotFoundException{
+		User user = userRepository.findByUsername(username);
+		
+		if(user ==null) {
+			
+			throw new UsernameNotFoundException("user not found");
+		}
+		return user;
+		
+		
+	}
+
+}
